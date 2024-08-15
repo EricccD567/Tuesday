628c716d01b2640ebcc20066f6330deadc7b308f
ericcd <eric.dai2194@gmail.com>
2024-08-16 05:55:59 +1000

    Changed routing from relative to absolute

--- app.py
+++ app.py
@@ -8,16 +8,16 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-from .routes.auth_routes import auth as auth_bp
+from routes.auth_routes import auth as auth_bp
 app.register_blueprint(auth_bp)
 
-from .routes.task_routes import task as task_bp
+from routes.task_routes import task as task_bp
 app.register_blueprint(task_bp)
 
-from .routes.user_routes import user as user_bp
+from routes.user_routes import user as user_bp
 app.register_blueprint(user_bp)
 
-from .routes.connection_routes import connection as connection_bp
+from routes.connection_routes import connection as connection_bp
 app.register_blueprint(connection_bp)
 
 if __name__ == '__main__':
